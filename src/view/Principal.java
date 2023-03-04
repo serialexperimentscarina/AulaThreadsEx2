70141fa3c1709b693c253ffcbb37152202c24bce
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-04 13:36:00 -0300

    feat: Create Principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,8 +1,20 @@
 package view;
 
+import controller.ThreadMatriz;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		int[][] matriz = new int[3][5];
+		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 5; j++) {
+				matriz[i][j] = (int) (Math.random() * 101);
+			}
+		
+			Thread threadMatriz = new ThreadMatriz(matriz[i], i);
+			threadMatriz.start();
+		}
 
 	}
 
